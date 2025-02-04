d71dc644a8866967ab4492314edb887a1815ac74
bulut yerli <bulutyerli@gmail.com>
2025-02-04 12:38:04 +0300

    Create useCurrentLocation hook

---
+++ src/hooks/useCurrentLocation/useCurrentLocation.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+type LocationType = {
+  latitude: number;
+  longitude: number;
+} | null;
+
+export default function useCurrentLocation() {
+  const [location, setLocation] = useState<LocationType>(null);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError("Geolocation is not supported");
+      return;
+    }
+
+    const handleSuccess = (position: GeolocationPosition) => {
+      const { latitude, longitude } = position.coords;
+      setLocation({ latitude, longitude });
+    };
+
+    const handleError = (err: GeolocationPositionError) => {
+      setError(err.message);
+    };
+
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+  }, []);
+
+  return { location, error };
+}
